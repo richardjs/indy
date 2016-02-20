1a3b875aaa2bd3609d4cbff9a6ce82fb28b9e59a
Richard Schneider <richard@schneiderbox.net>
2016-02-20 11:13:24 -0600

    fix depth 1 block test

--- test.sh
+++ test.sh
@@ -5,7 +5,12 @@ if [ "$move" != "A5-C2" ]; then
 	echo "falied basic win"
 fi
 
-move=$(./indy 00111000001000122222002)
+move=$(./indy 10001120001002122200002)
 if [ "$move" != "D1-C2" ]; then
-	echo "falied basic block"
+	echo "falied depth 1 block"
+fi
+
+move=$(./indy 10110001120200000222002)
+if [ "$move" != "B5-B2" ]; then
+	echo "falied depth 2 block"
 fi
