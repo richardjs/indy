d93e1dbb02ac15951198ac3cbb2846a32ddeb19a
Richard Schneider <richard@schneiderbox.net>
2016-02-21 22:22:23 -0600

    make test failure more visible

--- test.sh
+++ test.sh
@@ -2,15 +2,15 @@
 
 move=$(./indy 00111000001000122222001)
 if [ "$move" != "A5-C2" ]; then
-	echo "falied basic win"
+	echo "##FALIED BASIC WIN##"
 fi
 
 move=$(./indy 10001120001002122200002)
 if [ "$move" != "D1-C2" ]; then
-	echo "falied depth 1 block"
+	echo "##FALIED DEPTH 1 Block##"
 fi
 
 move=$(./indy 10110001120200000222002)
 if [ "$move" != "B5-B2" ]; then
-	echo "falied depth 2 block"
+	echo "##FALIED DEPTH 2 Block##"
 fi
